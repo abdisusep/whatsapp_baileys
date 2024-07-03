6a353fdd4e2eb78edd7797c5edb554c7a0dca0f4
Susep Dev <susep.dev@gmail.com>
2024-07-03 16:57:38 +0700

    2024-07-03 16:57

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const {
 } = require('@whiskeysockets/baileys');
 
 const pino = require('pino');
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let userId = 'dev';
+let sock;
+let qrValue;
 
 const connectToWhatsApp = async () => {
-	const userId = 'dev';
     const { state, saveCreds } = await useMultiFileAuthState(`sessions/${userId}`);
 
-    const sock = makeWASocket({
+    sock = makeWASocket({
     	printQRInTerminal: true,
     	auth: state,
     	logger: pino({ level: 'silent' })
@@ -18,6 +29,10 @@ const connectToWhatsApp = async () => {
 
     sock.ev.on('connection.update', async (update) => {
     	const { connection, lastDisconnect, qr } = update;
+    	
+    	if (qr !== undefined) {
+    		qrValue = qr;
+    	}
 
     	if (connection === 'close') {
     		const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
@@ -41,4 +56,58 @@ const connectToWhatsApp = async () => {
     sock.ev.on('creds.update', saveCreds);
 }
 
+app.get('/', (req, res) => {
+  	const response = {
+        success: true,
+        status: qrValue ? 'Scan QR' : 'Connected',
+        qr: qrValue
+    };
+
+    res.json(response);
+})
+
+app.get('/send', async (req, res) => {
+    const { number, message } = req.query;
+
+    if (!number || !message) {
+        return res.status(200).json({
+            success: false,
+            message: 'Number and message required!',
+        });
+    }
+
+    let numberWA;
+    const serverWA = '@c.us';
+    if (number.startsWith('62')) {
+      numberWA = number + serverWA;
+    } else {
+      numberWA = '62' + number.substring(1) + serverWA;
+    }
+
+    try {
+        const exists = await sock.onWhatsApp(numberWA);
+        if (exists?.jid || (exists && exists[0]?.jid)) {
+            await sock.sendMessage(exists.jid || exists[0].jid, { text: message });
+            res.status(200).json({
+                success: true,
+                message: `Message send successfull to ${number}`,
+            });
+        } else {
+            res.status(200).json({
+                success: false,
+                message: `Number ${number} not registered!`,
+            });
+        }
+    } catch (err) {
+        res.status(200).json({
+            success: false,
+            message: err.message,
+        });
+    }
+});
+
 connectToWhatsApp();
+
+app.listen(port, () => {
+  console.log(`Running on port : ${port}`)
+})
